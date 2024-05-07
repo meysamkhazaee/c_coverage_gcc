985d7a19678ce11a9bd046bd7e4c72707ac550db
Meysam Khazaee Sabor <57112732+Meysam-khazaee@users.noreply.github.com>
2024-05-07 12:19:55 +0330

    update script

--- runner.sh
+++ runner.sh
@@ -1,63 +1,71 @@
 # Change to the current directory
-cd "$PWD"
+cd $PWD
 
 # Remove the "result" folder if it exists
-if [ -d "result" ]; then
-    rm -rf "result"
+if [ -d result ]; then
+    rm -rf result
 fi
 
 # Create a new "result" folder
-mkdir "result"
-cp sample.c "result"
-cd "result"
+mkdir result
+cp sample.c input1.txt result/
+cd result
 
 # Build the code without coverage and instrumentation
-gcc -o 1-build_nocoverage_and_noinstrument.exe sample.c
-./1-build_nocoverage_and_noinstrument.exe 10 20 -3
+gcc -o not_cover_and_not_instrument.exe sample.c
+./not_cover_and_not_instrument.exe input1.txt > run_result.txt
 
 # Move all files in the current directory to the new subfolder, except for the excluded file
 for file in $(ls); do
-  if [[ ! -d "1-build_nocoverage_and_noinstrument" ]]; then
+  if [[ ! -d "not_cover_and_not_instrument" ]]; then
     # Create the directory
-    mkdir "1-build_nocoverage_and_noinstrument"
+    mkdir "not_cover_and_not_instrument"
   fi
-  if [[ "$file" != "sample.c" ]] && [[ "$file" != "1-build_nocoverage_and_noinstrument/" ]]; then
-    mv "$file" "1-build_nocoverage_and_noinstrument"
+  if [[ "$file" != "sample.c" ]] && [[ "$file" != "input1.txt" ]] && [[ "$file" != "not_cover_and_not_instrument/" ]]; then
+    mv "$file" "not_cover_and_not_instrument"
   fi
 done
 
+rm input1.txt
+cd ..
+cp input2.txt result/
+cd result
 # Build the code with coverage and without instrumentation
-gcc -o 2-build_coverage_and_noinstrument.exe sample.c --coverage
-./2-build_coverage_and_noinstrument.exe 10 20 -3
+gcc -o cover_and_not_instrument.exe sample.c --coverage
+./cover_and_not_instrument.exe input2.txt > run_result2.txt
 gcov sample.c
 gcovr -r . --html-details -o coverage_report_coverage_and_noinstrument.html
 gcovr --print-summary --object-directory . > coverage_report.txt
 
 # Move all files in the current directory to the new subfolder, except for the excluded file
 for file in $(ls); do
-  if [[ ! -d "2-build_coverage_and_noinstrument" ]]; then
+  if [[ ! -d "cover_and_not_instrument" ]]; then
     # Create the directory
-    mkdir "2-build_coverage_and_noinstrument"
+    mkdir "cover_and_not_instrument"
   fi
-  if [[ "$file" != "sample.c" ]] && [[ "$file" != "1-build_nocoverage_and_noinstrument/" ]] && [[ "$file" != "2-build_coverage_and_noinstrument/" ]]; then
-    mv "$file" "2-build_coverage_and_noinstrument"
+  if [[ "$file" != "sample.c" ]] && [[ "$file" != "input2.txt" ]] && [[ "$file" != "not_cover_and_not_instrument" ]] && [[ "$file" != "cover_and_not_instrument" ]]; then
+    mv "$file" "cover_and_not_instrument"
   fi
 done
 
+rm input2.txt
+cd ..
+cp input3.txt result/
+cd result
 # Build the code with coverage and instrumentation
-gcc -fprofile-arcs -ftest-coverage -g -o 3-build_coverage_and_instrument.exe sample.c
-./3-build_coverage_and_instrument.exe 10 20 -3
+gcc -fprofile-arcs -ftest-coverage -g -o cover_and_instrument.exe sample.c
+./cover_and_instrument.exe input3.txt > run_result3.txt
 gcov sample.c
 gcovr -r . --html-details -o coverage_report_coverage_and_instrument.html
 
 # Move all files in the current directory to the new subfolder, except for the excluded file
 for file in $(ls); do
-  if [[ ! -d "3-build_coverage_and_instrument" ]]; then
+  if [[ ! -d "cover_and_instrument" ]]; then
     # Create the directory
-    mkdir "3-build_coverage_and_instrument"
+    mkdir "cover_and_instrument"
   fi
-  if [[ "$file" != "sample.c" ]] && [[ "$file" != "1-build_nocoverage_and_noinstrument/" ]] && [[ "$file" != "2-build_coverage_and_noinstrument/" ]] && [[ "$file" != "3-build_coverage_and_instrument/" ]]; then
-    mv "$file" "3-build_coverage_and_instrument"
+  if [[ "$file" != "sample.c" ]] && [[ "$file" != "input3.txt" ]] && [[ "$file" != "not_cover_and_not_instrument" ]] && [[ "$file" != "cover_and_not_instrument" ]] && [[ "$file" != "cover_and_instrument" ]]; then
+    mv "$file" "cover_and_instrument"
   fi
 done
 
--- sample.c
+++ sample.c
@@ -3,38 +3,53 @@
 
 int main(int argc, char *argv[]) 
 {
-  if (argc != 4) 
-  {
+  printf("Program name: %s\n", argv[0]);
+  FILE *fptr;
+  int num1;
+  int num2;
+  int num3;
+  if (argc < 2) {
+    printf("Usage: %s .not input\n", argv[0]);
+    return 1;
+  }
+  else if(argc == 2){
+    printf("Number of arguments: %d\nRead From File\n", argc - 1);
+    fptr = fopen(argv[1], "r");
+    fscanf(fptr, "%d", &num1);
+    fscanf(fptr, "%d", &num2);
+    fscanf(fptr, "%d", &num3);
+  }
+  else if (argc == 4) {
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
+    num3 = atoi(argv[3]);
+  }
+  else{
     printf("Invalid number of arguments. Please provide three numbers.\n");
     return 1;
   }
 
-  int num1 = atoi(argv[1]);
-  int num2 = atoi(argv[2]);
-  int num3 = atoi(argv[3]);
-
   // Compare num1 and num2
-  if (num1 > num2) 
-  {
-    printf("%d is greater than %d.\n", num1, num2);
-  } else if (num1 < num2) 
-  {
-    printf("%d is less than %d.\n", num1, num2);
-  } else 
-  {
+  if (num1 > num2) {
+    printf("num1 = %d is greater than num2 = %d.\n", num1, num2);
+  } 
+  else if (num1 < num2) {
+    printf("num1 = %d is less than num2 = %d.\n", num1, num2);
+  } 
+  else {
     printf("Both numbers are equal.\n");
   }
 
   // Check num3 for positivity or negativity
   if (num3 > 0) 
   {
-    printf("The number is positive.\n");
+    printf("%d is positive.\n", num3);
   } else if (num3 < 0) 
   {
-    printf("The number is negative.\n");
+    printf("%d is negative.\n", num3);
   } else 
   {
-    printf("The number is zero.\n");
+    printf("%d is zero.\n", num3);
   }
 
   return 0;
